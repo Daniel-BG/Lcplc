5176d81c1cd157b53bba803200b25a16ed500354
Daniel-BG <danielbasgar@gmail.com>
2019-04-25 12:47:29 +0200

    Added switch to remove clutter from the SW compression to get a real time when executing

--- src/lcplc/Main.java
+++ src/lcplc/Main.java
@@ -42,6 +42,9 @@ public class Main {
 
 	public static class Compressor {
 		
+		private static final boolean FAST_COMPRESS = true;
+		
+		
 		private static final int CONST_ACC_QUANT = 32;
 		//delta = down/up
 		private static final boolean CONST_Q_ONLYSHIFT = true;
@@ -52,7 +55,7 @@ public class Main {
 		private static final int CONST_GAMMA = 0;
 		
 		
-		private static final int BLOCKS_TO_CODE = 3;
+		private static final int BLOCKS_TO_CODE = 200;
 		private static final int MAX_LINES_PER_BLOCK = 16;
 		private static final int MAX_SAMPLES_PER_BLOCK = 16;
 		private static final int SAMPLE_DEPTH = 16;
@@ -84,6 +87,11 @@ public class Main {
 			ByteArrayOutputStream rawBinBaos = new ByteArrayOutputStream();
 			BitOutputStream rawBinIn = new BitOutputStream(rawBinBaos);
 			
+			///////
+			//get starting time here
+			long sTime = System.nanoTime();
+			///////
+			
 			int compressedBlocks = 0;
 			for (int l = 0; l < imgLines; l += MAX_LINES_PER_BLOCK) {
 				for (int s = 0; s < imgSamples; s += MAX_SAMPLES_PER_BLOCK) {
@@ -97,20 +105,27 @@ public class Main {
 						for (int ll = 0; ll < blockLines; ll++) {
 							for (int ss = 0; ss < blockSamples; ss++) {
 								block[bb][ll][ss] = hid.getValueAt(bb, ll+l, ss+s);
-								try {
-									rawBinIn.writeBits(hid.getValueAt(bb, ll+l, ss+s), 16, BitStreamConstants.ORDERING_LEFTMOST_FIRST);
-								} catch (IOException e) {e.printStackTrace();}
+								if (!FAST_COMPRESS) { 
+									try {
+										rawBinIn.writeBits(hid.getValueAt(bb, ll+l, ss+s), 16, BitStreamConstants.ORDERING_LEFTMOST_FIRST);
+									} catch (IOException e) {e.printStackTrace();}
+								}
 							}
 						}
 					}
 					//compress block
 					try {
-						this.compress(block, blockBands, blockLines, blockSamples, compressedBlocks == BLOCKS_TO_CODE - 1,  bos);
+						if (FAST_COMPRESS) {
+							this.fastCompress(block, blockBands, blockLines, blockSamples, compressedBlocks == BLOCKS_TO_CODE - 1,  bos);
+						} else {
+							this.compress(block, blockBands, blockLines, blockSamples, compressedBlocks == BLOCKS_TO_CODE - 1,  bos);
+						}
 					} catch (IOException e) {
 						e.printStackTrace();
 						System.exit(0);
 					}
-					System.out.println("COMPR:  " + bos.getBitsOutput());
+					if (!FAST_COMPRESS)
+						System.out.println("COMPR:  " + bos.getBitsOutput());
 					compressedBlocks++;
 					if (compressedBlocks >= BLOCKS_TO_CODE)
 						break;
@@ -125,6 +140,16 @@ public class Main {
 				e1.printStackTrace();
 				System.exit(0);
 			}
+			
+			///////
+			//get ending time here
+			long eTime = System.nanoTime();
+			long time = eTime - sTime;
+			double secondTime = ((double) time) / 1000000000.0;
+			System.out.print("Total compress time is: " + secondTime + "\n");
+			sTime = System.nanoTime();
+			///////
+			
 			
 			ByteArrayInputStream adbais = new ByteArrayInputStream(adbaos.toByteArray());
 			BitInputStream bis = new BitInputStream(adbais);
@@ -144,8 +169,8 @@ public class Main {
 						e.printStackTrace();
 						System.exit(0);
 					}
-
-					System.out.println("UNCOMPR:  " + bis.getBitsInput());
+					if (!FAST_COMPRESS)
+						System.out.println("UNCOMPR:  " + bis.getBitsInput());
 					unCompressedBlocks++;
 					if (unCompressedBlocks >= BLOCKS_TO_CODE)
 						break;
@@ -153,6 +178,14 @@ public class Main {
 				if (unCompressedBlocks >= BLOCKS_TO_CODE)
 					break;
 			}
+			
+			///////
+			//get ending time here
+			eTime = System.nanoTime();
+			time = eTime - sTime;
+			secondTime = ((double) time) / 1000000000.0;
+			System.out.print("Total uncompress time is: " + secondTime + "\n");
+			///////
 			
 			
 			//output bytes
@@ -541,6 +574,153 @@ public class Main {
 			
 			expGolombZero.endSampling();
 			golombCoDec.endSampling();
+		}
+		
+		
+		public void fastCompress(int[][][] block, int bands, int lines, int samples, boolean flushBlock, BitOutputStream bos) throws IOException {
+			int[][][] decodedBlock = new int[bands][lines][samples];
+			
+			ExpGolombCoDec expGolombZero = new ExpGolombCoDec(0);
+			GolombCoDec golombCoDec = new GolombCoDec(0);
+			Accumulator acc = new Accumulator(CONST_ACC_QUANT);
+			Quantizer quantizer;
+			if (CONST_Q_ONLYSHIFT)
+				quantizer = new ShifterQuantizer(CONST_SQ_DOWNSCALE);
+			else
+				quantizer = new IntegerUniformThresholdQuantizer(CONST_UTQ_DOWNSCALE, CONST_UTQ_UPSCALE); 
+			
+			//compress first band
+			int[][] band = block[0];
+			for (int l = 0; l < lines; l++) {
+				for (int s = 0; s < samples; s++) {	
+					//First sample is just coded raw since we have not initialized
+					//the counters/accumulators/predictors yet
+					int error = 0;
+					int mappedError;
+					int prediction;
+					int kj = 0;
+					if (l == 0 && s == 0) {
+						int quant = quantizer.quantize(block[0][l][s]);
+						int dequant = quantizer.dequantize(quant);
+						mappedError = Mapper.mapError(quant); //Mapper.mapError(block[0][l][s]);
+						expGolombZero.encode(mappedError, bos);
+						//mimic hw by injecting here the first sample
+						 
+						prediction = 0;
+						
+						decodedBlock[0][l][s] = dequant;
+						acc.add(dequant);
+						
+					//For every other sample, code following
+					//the predictive scheme
+					} else {
+						prediction = Predictor.basic2DPrediction(decodedBlock[0], l, s);
+						
+						
+						error = block[0][l][s] - (int) prediction;
+						int qErr  = quantizer.quantize(error);
+						error 	  = quantizer.dequantize(qErr);
+						decodedBlock[0][l][s] = (int) prediction + error;
+
+						kj = findkj(acc);
+						
+						//code mapped error
+						mappedError = Mapper.mapError(qErr);
+						golombCoDec.encode(kj, mappedError, bos);	//encode the error
+						acc.add(Math.abs(error));		//update Rj after coding
+					}
+				}
+			}
+			
+			//calculate distortion threshold
+			long sampleCnt = lines*samples;
+			double delta = (double) CONST_UTQ_DOWNSCALE / (double) CONST_UTQ_UPSCALE;
+			double thres = (double) CONST_GAMMA * delta * delta * sampleCnt * sampleCnt / 3.0;
+			
+			//compress rest of bands
+			for (int b = 1; b < bands; b++) {
+				band = block[b];
+				//generate means. we'll see where these means have to be from
+				long currAcc = Utils.sumArray(band, lines, samples);
+				long prevAcc = Utils.sumArray(decodedBlock[b-1], lines, samples);
+				
+				//generate alpha value. Could try to generate it using the original band as well to see performance
+				long simpleAlphaNacc = 0;
+				long simpleAlphaDacc = 0;
+				for (int l = 0; l < lines; l++) {
+					for (int s = 0; s < samples; s++) {
+						simpleAlphaNacc += (decodedBlock[b-1][l][s] - prevAcc/sampleCnt)*(band[l][s] 			  - currAcc/sampleCnt);
+						simpleAlphaDacc += (decodedBlock[b-1][l][s] - prevAcc/sampleCnt)*(decodedBlock[b-1][l][s] - prevAcc/sampleCnt);
+					}
+				}
+				
+				//allocate 10 bits for alpha (when using it we need to divide by 512
+				//to stay in the [0, 2) range
+				int simpleAlphaScaled = findAlpha(simpleAlphaNacc, simpleAlphaDacc, 10);
+				long alphaScaleVal = 9; //512;
+				//mu is 16 bits wide, and should stay that way since we are averaging 16-bit values
+				long muScaled = currAcc / sampleCnt;
+
+				//save encoding data in these variables and only add
+				//it if we don't skip the block
+				int[][] savedMappedError = new int[lines][samples];
+				int[][] savedGolombParam = new int[lines][samples];
+				int[][] savedPrediction  = new int[lines][samples];
+				//initialize values for block compression
+				long distortionAcc = 0;
+				acc.reset(); 
+				int[][] savedxhat = new int[lines][samples];
+				for (int l = 0; l < lines; l++) {
+					for (int s = 0; s < samples; s++) {
+						long prediction = muScaled + (((decodedBlock[b-1][l][s] - prevAcc/sampleCnt)*simpleAlphaScaled)>>alphaScaleVal);
+						savedPrediction[l][s] = (int) prediction;
+						
+						int error = band[l][s] - (int) prediction;
+						distortionAcc += error*error;
+						
+						//quantize error and replace it with the
+						//unquantized version since this is the one
+						//the decoder will have
+						int qErr  = quantizer.quantize((int) error);
+						error 	  = quantizer.dequantize(qErr);
+						long mappedError = Mapper.mapError(qErr);
+						savedMappedError[l][s] = (int) mappedError;
+						
+						savedxhat[l][s] = (int) prediction + (int) error;
+						
+						if (l != 0 || s != 0) {
+							savedGolombParam[l][s] = findkj(acc);
+						}
+						acc.add(Math.abs(error));		//update Rj after coding
+					}
+				}
+				
+				//write flag and alpha and mu
+				bos.writeBit(Bit.fromBoolean(distortionAcc > thres));
+				bos.writeBits((int) simpleAlphaScaled, 10, BitStreamConstants.ORDERING_LEFTMOST_FIRST);
+				bos.writeBits((int) muScaled, 16, BitStreamConstants.ORDERING_LEFTMOST_FIRST);
+				
+				if (distortionAcc > thres) {
+					//code block as normal
+					for (int l = 0; l < lines; l++) {
+						for (int s = 0; s < samples; s++) {
+							decodedBlock[b][l][s] = Utils.clamp(savedxhat[l][s], 0, (1 << SAMPLE_DEPTH) - 1);
+							if (l == 0 && s == 0) {
+								expGolombZero.encode(savedMappedError[l][s], bos);
+							} else {
+								golombCoDec.encode(savedGolombParam[l][s], savedMappedError[l][s], bos);	//encode the error
+							}
+						}
+					}
+				} else {
+					//skip block
+					for (int l = 0; l < lines; l++) {
+						for (int s = 0; s < samples; s++) {
+							decodedBlock[b][l][s] = Utils.clamp(savedPrediction[l][s], 0, (1 << SAMPLE_DEPTH) - 1);
+						}
+					}
+				}
+			}
 		}
 		
 		public int findAlpha(long alphaN, long alphaD, long depth) {
